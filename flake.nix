7bc3280c12cc7667732570715dd7c401c0d64fd3
Alex Ameen <alex.ameen.tx@gmail.com>
2022-02-23 19:12:33 -0600

    update flake lock

--- flake.nix
+++ flake.nix
@@ -1,14 +1,13 @@
 {
   description = "simple file manager";
-  inputs.nixpkgs.follows = "nix/nixpkgs";
-  outputs = { self, nix, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }: {
 
     overlays.sfm = import ./overlay.nix;
     overlay = self.overlays.sfm;
 
     packages.x86_64-linux.sfm = ( import nixpkgs {
       sys = "x86_64-linux";
-      overlays = [self.overlay nix.overlay];
+      overlays = [self.overlay];
     } ).sfm;
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.sfm;
 
